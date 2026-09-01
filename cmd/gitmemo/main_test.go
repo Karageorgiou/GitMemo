cd8db32a73838e571e22dd186fa01a2c953796a1
George Karageorgiou <karageorgiou.geo@gmail.com>
2026-09-02 00:00:32 +0300

    test: cover trust version bootstrap command

--- cmd/gitmemo/main_test.go
+++ cmd/gitmemo/main_test.go
@@ -16,6 +16,16 @@ func TestInitCommandCreatesRepository(t *testing.T) {
 	}
 }
 
+func TestTrustVersionReadsInitializedRepository(t *testing.T) {
+	root := filepath.Join(t.TempDir(), "memory")
+	if code := run([]string{"init", root}); code != 0 {
+		t.Fatalf("init exit code = %d", code)
+	}
+	if code := run([]string{"trust", "version", root}); code != 0 {
+		t.Fatalf("trust version exit code = %d", code)
+	}
+}
+
 func TestInitCommandRejectsTooManyTargets(t *testing.T) {
 	if code := run([]string{"init", "one", "two"}); code != 2 {
 		t.Fatalf("init exit code = %d, want 2", code)
